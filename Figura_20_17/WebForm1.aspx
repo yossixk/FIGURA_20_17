50ea34bd645d14133d18f925fd8a794cbf0072b9
Yoss <gironandrea544gmail.com>
2025-04-11 06:36:44 -0600

    Primer commit

---
+++ Figura_20_17/WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Figura_20_17.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:SqlDataSource runat="server" ID="sdsListBoxProduct" 
+                ConnectionString="<%$ConnectionStrings:SportsPlay%>"
+                SelectCommand="SELECT ProductID, Product 
+                                FROM Product
+                                ORDER BY Product;">
+            </asp:SqlDataSource>
+
+            <asp:Label runat="server" Text="Product"></asp:Label><br />
+            <asp:ListBox runat="server" ID="libProduct" 
+                DataSourceID="sdsListBoxProduct" DataTextField="Product"
+                DataValueField="ProductID" Rows="7">
+            </asp:ListBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Figura_20_17/WebForm1.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Figura_20_17
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control sdsListBoxProduct.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource sdsListBoxProduct;
+
+        /// <summary>
+        /// Control libProduct.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox libProduct;
+    }
+}
